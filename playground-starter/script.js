6910585b6f751f403fa297655e1c293f312effcb
FallowBlade <113985916+FallowBlade@users.noreply.github.com>
2023-01-31 18:23:17 +0800

    Update script.js

--- playground-starter/script.js
+++ playground-starter/script.js
@@ -1,1 +1,31 @@
-console.log("Hello, world!");
+// let username = "Isabelle Lange";
+// console.log(`Hello, ${username}`);
+
+// let num1 = 3;
+// let num2 = 2;
+// let total = num1 + num2;
+// console.log(total);
+
+// function example(){
+//     console.log("G'day mate, hope you are functionally OK!");
+// }
+
+// example();
+
+// function add() {
+//     let x = 5;
+//     let y = 3;
+//     let result = x + y;
+//     console.log(result);
+// }
+
+// add();
+
+function add(x, y) {
+    let result = x + y;
+    console.log(result);
+}
+
+add(5, 3);
+add(10, 3)
+add(100, 200)
